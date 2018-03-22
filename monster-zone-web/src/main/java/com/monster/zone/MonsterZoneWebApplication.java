38d0f6d605c7b5c5ed782a19eee4db7676da0c8a
monster <r_wushimin@163.com>
2018-03-22 23:33:21 +0800

    init

---
+++ monster-zone-web/src/main/java/com/monster/zone/MonsterZoneWebApplication.java
@@ -0,0 +1,12 @@
+package com.monster.zone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsterZoneWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonsterZoneWebApplication.class, args);
+	}
+}
